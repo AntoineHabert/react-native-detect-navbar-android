e70d3ccc68343ac5e0a163f18fe585ea6e567622
Antoine Habert <>
2020-06-30 10:54:48 +0200

    return difference between screen height and window height in dp

--- android/src/main/java/com/rndetectnavbarandroid/RNDetectNavbarAndroidModule.java
+++ android/src/main/java/com/rndetectnavbarandroid/RNDetectNavbarAndroidModule.java
@@ -1,21 +1,5 @@
 package com.rndetectnavbarandroid;
-/*
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.annotation.SuppressLint;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.KeyCharacterMap;
-import android.util.DisplayMetrics;
-import android.view.KeyEvent;
-import android.view.ViewConfiguration;
-import android.view.Display;
-import android.content.Context;
-import android.util.Log;
-*/
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -35,7 +19,7 @@ import android.view.WindowManager;
 /**
  * {@link NativeModule} that allows changing the appearance of the menu bar.
  */
-public class RNDetectNavbarAndroidModule extends ReactContextBaseJavaModule { 
+public class RNDetectNavbarAndroidModule extends ReactContextBaseJavaModule {
 
   ReactApplicationContext reactContext;
 
@@ -49,90 +33,57 @@ public class RNDetectNavbarAndroidModule extends ReactContextBaseJavaModule {
     return "RNDetectNavbarAndroid";
   }
 
-  /*
-  @Override
-  public Map<String, Object> getConstants() {
-      Map<String, Object> constants = new HashMap<>();
-  
-      constants.put("has_soft_keys", hasSoftKeys());
-  
-      return constants;
+  private float convertPixelsToDp(float px) {
+    return px / (this.reactContext.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
   }
-*/
 
-  private boolean _hasSoftKeys() {
+  private float _hasSoftKeys() {
     boolean hasSoftwareKeys;
 
-    /*
-    Activity activity = getCurrentActivity();
-
-    if (activity == null) {
-        return true;
-    }
-*/
     WindowManager window = getCurrentActivity().getWindowManager();
 
-    if(window != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1){
-        Display d = getCurrentActivity().getWindowManager().getDefaultDisplay();
+    if (window != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+      Display d = getCurrentActivity().getWindowManager().getDefaultDisplay();
 
-        DisplayMetrics realDisplayMetrics = new DisplayMetrics();
-        d.getRealMetrics(realDisplayMetrics);
+      DisplayMetrics realDisplayMetrics = new DisplayMetrics();
+      d.getRealMetrics(realDisplayMetrics);
 
-        int realHeight = realDisplayMetrics.heightPixels;
-        int realWidth = realDisplayMetrics.widthPixels;
+      int realHeight = realDisplayMetrics.heightPixels;
+      int realWidth = realDisplayMetrics.widthPixels;
 
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        d.getMetrics(displayMetrics);
+      //System.out.println("JAVA realHeight " + realHeight);
+      //System.out.println("JAVA realWidth " + realWidth);
 
-        int displayHeight = displayMetrics.heightPixels;
-        int displayWidth = displayMetrics.widthPixels;
+      DisplayMetrics displayMetrics = new DisplayMetrics();
+      d.getMetrics(displayMetrics);
 
-        hasSoftwareKeys =  (realWidth - displayWidth) > 0 || (realHeight - displayHeight) > 0;
-    } else {
-        boolean hasMenuKey = ViewConfiguration.get(this.reactContext).hasPermanentMenuKey();
-        boolean hasBackKey = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK);
+      int displayHeight = displayMetrics.heightPixels;
+      int displayWidth = displayMetrics.widthPixels;
 
-        hasSoftwareKeys = !hasMenuKey && !hasBackKey;
+      System.out.println("JAVA displayHeight " + displayHeight);
+      System.out.println("JAVA displayWidth " + displayWidth);
+
+      hasSoftwareKeys = (realWidth - displayWidth) > 0 || (realHeight - displayHeight) > 0;
+
+      //System.out.println("JAVA hasSoftwareKeys " + hasSoftwareKeys);
+
+      return (this.convertPixelsToDp(realHeight - displayHeight));
+
+    } else { // not supported anymore in our apps anyway
+      boolean hasMenuKey = ViewConfiguration.get(this.reactContext).hasPermanentMenuKey();
+      boolean hasBackKey = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK);
+
+      System.out.println("JAVA hasMenuKey " + hasMenuKey);
+      System.out.println("JAVA hasBackKey " + hasBackKey);
+
+      hasSoftwareKeys = !hasMenuKey && !hasBackKey;
+
+      return 0;
     }
-
-    return hasSoftwareKeys;
-}
+  }
 
   @ReactMethod
-  public void hasSoftKeys(Promise promise) {    
+  public void hasSoftKeys(Promise promise) {
     promise.resolve(_hasSoftKeys());
-  }  
-/*
-  private static boolean hasImmersive;
-  private static boolean cached = false;
-
-  @SuppressLint ("NewApi")
-  private boolean hasImmersive() {
-
-      if (!cached) {
-          if(Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-              hasImmersive = false;
-              cached = true;
-              return false;
-          }
-          Display d = ((WindowManager) reactContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-
-          DisplayMetrics realDisplayMetrics = new DisplayMetrics();
-          d.getRealMetrics(realDisplayMetrics);
-
-          int realHeight = realDisplayMetrics.heightPixels;
-          int realWidth = realDisplayMetrics.widthPixels;
-
-          DisplayMetrics displayMetrics = new DisplayMetrics();
-          d.getMetrics(displayMetrics);
-
-          int displayHeight = displayMetrics.heightPixels;
-          int displayWidth = displayMetrics.widthPixels;
-
-          hasImmersive = (realWidth > displayWidth) || (realHeight > displayHeight);
-          cached = true;
-      }
-
-      return hasImmersive;
-  }*/
+  }
 }
